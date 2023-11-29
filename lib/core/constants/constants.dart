4c102635b08f5d2108535694f1b4ba6915b1acd2
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2023-11-29 16:49:32 +0300

    api request constants added

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = '503b97570e4249b198bcea24111ea1ac';
+const String countryQuery = 'tr';
+const String categoryQuery = 'general';
