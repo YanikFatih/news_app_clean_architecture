8c624b22905e67acddda99adfd232e12e7a6d0ad
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2024-08-10 13:56:23 +0300

    unnecessary imports removed

--- lib/features/daily_news/presentation/pages/home/daily_news.dart
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
-import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/widgets/article_tile.dart';
 
 class DailyNews extends StatelessWidget {
   const DailyNews({super.key});
@@ -39,8 +39,8 @@ class DailyNews extends StatelessWidget {
         if (state is RemoteArticlesDone) {
           return ListView.builder(
             itemBuilder: (context, index){
-              return ListTile(
-                title: Text("$index"),
+              return ArticleWidget(
+                article: state.articles![index],
               );
             },
             itemCount: state.articles!.length,
